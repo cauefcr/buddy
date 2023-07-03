b6cdd9aee0b48a5ca42e804a96a5a9c49ca462ab
cauef <cauef@proton.me>
2023-07-03 12:13:06 -0300

    fixed optimized gif playing

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/cauefcr/buddy
 go 1.16
 
 require (
-	github.com/hajimehoshi/ebiten/v2 v2.2.3
-	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
-	golang.org/x/sys v0.0.0-20211204120058-94396e421777 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220622232848-a6c407ee30a0 // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.3.4
+	gopkg.in/gographics/imagick.v3 v3.4.2
 )
--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"fmt"
 	"image"
 	"image/gif"
+	"os"
+	"runtime"
+	"sync"
 	"io/ioutil"
-	"log"
 	"math/rand"
 	"net/http"
-	"os"
 	"time"
 
 	ebiten "github.com/hajimehoshi/ebiten/v2"
+	im "gopkg.in/gographics/imagick.v3/imagick"
+)
+
+var (
+	choices = [][]int{}
 )
 
 type Game struct {
@@ -19,63 +27,79 @@ type Game struct {
 	duration []int
 	tick     int
 	curr     int
-	start    chan bool
+	lastUp   time.Time
 	pos      []image.Rectangle
 }
 
 func (g *Game) Update() error {
+	if 0 == len(gm.jiff) {
+		return nil
+	}
+	if gm.tick == 0 {
+		gm.lastUp = time.Now()
+	}
 	cx, cy := ebiten.CursorPosition()
 	w, h := ebiten.WindowSize()
 	if cx > 0 && cx < w && cy > 0 && cy < h {
 		die := rand.Int31n(4)
 		ebiten.SetWindowPosition(choices[die][0], choices[die][1])
 	}
-	g.tick++
-
+	if gm.lastUp.Before(time.Now()) {
+		gm.curr++
+		gm.curr %= len(gm.jiff)
+		gm.lastUp = time.Now().Add(time.Duration(gm.duration[gm.curr]) * time.Second / 100)
+	}
+	gm.tick++
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	opt := ebiten.DrawImageOptions{}
-	opt.GeoM.Translate(float64(g.pos[g.curr].Min.X), float64(g.pos[g.curr].Min.Y))
-	select {
-	case g.start <- true:
-	default:
+	if len(gm.jiff) == 0 {
+		g = zero
+		return
 	}
-	screen.DrawImage(g.jiff[g.curr], &opt)
+	opt := ebiten.DrawImageOptions{}
+	opt.GeoM.Translate(float64(gm.pos[gm.curr].Min.X), float64(gm.pos[gm.curr].Min.Y))
+	screen.DrawImage(gm.jiff[gm.curr], &opt)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return outsideWidth, outsideHeight
 }
 
-var choices [][]int
-
-func gameFromGif(path string) (*Game, int, int) {
+func gameFromGif(path string) (*Game, int, int, error) {
 	var (
 		data []byte
 		err  error
 	)
-	data, err = ioutil.ReadFile(os.Args[1])
+	data, err = ioutil.ReadFile(path)
 	if err != nil {
-		resp, err := http.Get(os.Args[1])
+		resp, err := http.Get(path)
 		if err != nil {
-			log.Fatal(err)
+			return nil, 0, 0, err
 		}
 		data, err = ioutil.ReadAll(resp.Body)
 		if err != nil {
-			log.Fatal(err)
-
+			return nil, 0, 0, err
 		}
 	}
+	mw := im.NewMagickWand()
+	defer mw.Destroy()
+	err = mw.ReadImageBlob(data)
+	if err != nil {
+		return nil, 0, 0, err
+	}
+	mw = mw.CoalesceImages()
+	defer mw.Destroy()
+	data = mw.GetImagesBlob()
 	jiff, err := gif.DecodeAll(bytes.NewReader(data))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
+		return nil, 0, 0, err
 	}
 	game := &Game{jiff: []*ebiten.Image{}}
 	game.duration = jiff.Delay
 	game.pos = []image.Rectangle{}
-	game.start = make(chan bool, 1)
 	for _, img := range jiff.Image {
 		i := ebiten.NewImageFromImage(img)
 		game.jiff = append(game.jiff, i)
@@ -84,39 +108,81 @@ func gameFromGif(path string) (*Game, int, int) {
 
 	w := jiff.Config.Width
 	h := jiff.Config.Height
-	return game, w, h
+	return game, w, h, nil
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("Usage: ./buddy <gif>")
+var doneChan = make(chan bool, 1)
+var (
+	gm *Game = &Game{
+		jiff:     []*ebiten.Image{},
+		duration: []int{},
+		pos:      []image.Rectangle{},
 	}
-	game, w, h := gameFromGif(os.Args[1])
-	vw, vh := ebiten.ScreenSizeInFullscreen()
-	choices = [][]int{
-		{0, 0},
-		{vw - w, 0},
-		{0, vh - h},
-		{vw - w, vh - h},
-	}
-	ebiten.SetWindowPosition(choices[0][0], choices[0][1])
-	ebiten.SetWindowSize(w, h)
+	zero   = gm
+	vw, vh int
+)
+
+func RunGame(gifChan chan string, wg *sync.WaitGroup) {
+	wg.Add(1)
+	ebiten.SetScreenClearedEveryFrame(true)
+	vw, vh = ebiten.ScreenSizeInFullscreen()
+	go func() {
+		doneChan <- true
+		for range doneChan {
+			file, ok := <-gifChan
+			if !ok {
+				doneChan <- true
+				continue
+			}
+
+			game, w, h, err := gameFromGif(file)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			if ebiten.IsWindowMinimized() {
+				ebiten.RestoreWindow()
+			}
+			gm = game
+			choices = [][]int{
+				{0, 0},
+				{vw - w, 0},
+				{0, vh - h},
+				{vw - w, vh - h},
+			}
+			ebiten.SetWindowSize(w, h)
+			ebiten.SetWindowTitle(file)
+			ebiten.SetWindowPosition(choices[0][0], choices[0][1])
+		}
+		wg.Done()
+	}()
 	ebiten.SetScreenTransparent(true)
 	ebiten.SetWindowDecorated(false)
 	ebiten.SetWindowFloating(true)
-	ebiten.SetWindowTitle(os.Args[1])
+	if err := ebiten.RunGame(gm); err != nil {
+		fmt.Println(err)
+	}
+	wg.Wait()
+}
+
+func main() {
+	runtime.LockOSThread()
+	gifChan := make(chan string, 100)
+	wg := &sync.WaitGroup{}
+	gifChan <- os.Args[1]
+	im.Initialize()
+	defer im.Terminate()
 	go func() {
-		for ready := range game.start {
-			if ready {
-				break
+		rdr := bufio.NewReader(os.Stdin)
+		for {
+			line, err := rdr.ReadString('\n')
+			if err != nil {
+				close(gifChan)
+				os.Exit(0)
 			}
-		}
-		for range time.Tick(time.Second / 100 * time.Duration(game.duration[game.curr])) {
-			game.curr++
-			game.curr %= len(game.jiff)
+			gifChan <- line[:len(line)-1]
+			doneChan <- true
 		}
 	}()
-	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err)
-	}
+	RunGame(gifChan, wg)
 }
